dc107cb8845c24a4e0fd74257fb6ac4127fef8ac
soenneker <jake@soenneker.com>
2026-08-29 14:16:57 -0500

    Improve public API XML documentation

--- src/Soenneker.Copper.HttpClients/Abstract/ICopperOpenApiHttpClient.cs
+++ src/Soenneker.Copper.HttpClients/Abstract/ICopperOpenApiHttpClient.cs
@@ -2,13 +2,16 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.Copper.HttpClients.Abstract;
-
 /// <summary>
 /// A .NET thread-safe singleton HttpClient for 
 /// </summary>
 public interface ICopperOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Copper OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Copper.HttpClients/Registrars/CopperOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Copper.HttpClients/Registrars/CopperOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class CopperOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="CopperOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddCopperOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class CopperOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="CopperOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddCopperOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
